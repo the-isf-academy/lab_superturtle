10250c2db44803ff009328baa4004a2ed516ef64
emma <eqbrown@isf.edu.hk>
2024-09-17 11:19:32 +0800

    chagne extension, init settings.py

--- extension.py
+++ extension.py
@@ -1,8 +1,21 @@
 from turtle import *
 from superturtle.animation import animate
+from superturtle.movement import fly
 from shapes import triangle, rectangle
+import settings
 
-for frame in animate(60, loop=True):
-    with frame.translate(start=[-200, 0], stop=[200, 0]):
-        with frame.rotate(start=0, stop=360):
-                rectangle(20, 20, 'coral')
+
+bgcolor(settings.BACKGROUND_COLOR)
+
+
+for frame in animate(settings.TOTAL_FRAMES, loop = True):
+    for frame in animate(settings.TOTAL_FRAMES//3, loop=False):
+        with frame.translate(start=[-200, 0], stop=[200, 0]):
+            with frame.rotate(start=0, stop=360):
+                rectangle(settings.RECTANGLE_HEIGHT, settings.RECTANGLE_WIDTH, settings.RECTANGLE_COLOR)
+
+    for frame in animate(settings.TOTAL_FRAMES//3, loop=False):
+        with frame.translate(start=[200, 0], stop=[-200, 0]):
+            with frame.scale(start=1, stop=2):
+                rectangle(settings.RECTANGLE_HEIGHT, settings.RECTANGLE_WIDTH, settings.RECTANGLE_COLOR)
+
